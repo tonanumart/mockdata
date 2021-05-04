cacd9c54d91a8d575942d954ca2a75db44898cb5
cnx-anumart <anumart.c@b-connex.net>
2021-05-04 19:58:25 +0700

    initial master

---
+++ MockData.swift
@@ -0,0 +1,42 @@
+//
+//  CustomerRootClass.swift
+//  Model Generated using http://www.jsoncafe.com/
+//  Created on May 4, 2021
+
+import Foundation
+
+class Customer {
+    var age : Int!
+    var name : String!
+    init(_ name : String,_ age : Int){
+        self.name = name
+        self.age = age
+    }
+
+}
+
+var dataSource : [Customer] = [Customer("Tiger Nixon",61),
+        Customer("Garrett Winters",63),
+        Customer("Ashton Cox",66),
+        Customer("Cedric Kelly",22),
+        Customer("Airi Satou",33),
+        Customer("Brielle Williamson",61),
+        Customer("Herrod Chandler",59),
+        Customer("Rhona Davidson",21),
+        Customer("Colleen Hurst",39),
+        Customer("Sonya Frost",23),
+        Customer("Jena Gaines",30),
+        Customer("Quinn Flynn",22),
+        Customer("Charde Marshall",36),
+        Customer("Haley Kennedy",43),
+        Customer("Tatyana Fitzpatrick",19),
+        Customer("Michael Silva",66),
+        Customer("Paul Byrd",64),
+        Customer("Gloria Little",59),
+        Customer("Bradley Greer",41),
+        Customer("Dai Rios",35),
+        Customer("Jenette Caldwell",30),
+        Customer("Yuri Berry",40),
+        Customer("Caesar Vance",21),
+        Customer("Doris Wilder",23)]
+
